8576fc483e5aca92e72fd72b10e1be4d99c6de09
GingerGigiCat <cat@gigicat.co.uk>
2026-04-04 18:39:14 +0100

    added python code for a random image path which can also be used as an api to get an image, as well as a main page which shows the random image on a gradient background

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+import flask
+import random
+
+from flask import url_for, send_file
+
+app = flask.Flask(__name__, static_folder="assets")
+
+@app.route("/random_image")
+def get_image():
+    image_list = os.listdir("assets")
+    new_image_list = []
+    for image in image_list:
+        new_image_list.append(f"./assets/{image}")
+    return send_file(random.choice(new_image_list), mimetype="image")
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+app.run()

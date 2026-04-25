83b2a3fc4a26bcb2f42e043a715b420676fae5ed
GingerGigiCat <cat@gigicat.co.uk>
2026-04-25 16:21:02 +0100

    yay works

--- app.py
+++ app.py
@@ -23,16 +23,21 @@ from urllib3.exceptions import ResponseError
 
 GOOGLE_PHOTOS_ALBUM_LINK = ""
 ZIP_FILE_LINK = ""
+remote_images = []
+remote_zip: RemoteZip
+is_zip_file_refresher_running = False
+
 def album_link_splitter(album_link):
     split_link = re.findall("[a-zA-Z0-9-_]+", album_link)
     return split_link[split_link.index("share")+1], split_link[split_link.index("key")+1]
 
 def zip_file_refresher():
     """this is a long-running function"""
-    global ZIP_FILE_LINK, GOOGLE_PHOTOS_ALBUM_LINK
-    album_id, album_key = album_link_splitter(GOOGLE_PHOTOS_ALBUM_LINK)
-    #print(album_id, album_key)
+    global ZIP_FILE_LINK, GOOGLE_PHOTOS_ALBUM_LINK, remote_zip, remote_images, is_zip_file_refresher_running
+    if is_zip_file_refresher_running: return
+    is_zip_file_refresher_running = True
     try:
+        album_id, album_key = album_link_splitter(GOOGLE_PHOTOS_ALBUM_LINK)
         link1 = f"https://photos.google.com/_/PhotosUi/data/batchexecute?rpcids=P3pCwd&source-path=%2Fshare%2F{album_id}"
         data1 = f"f.req=%5B%5B%5B%22P3pCwd%22%2C%22%5B%5B%5C%22{album_id}%5C%22%5D%2C%5C%22{album_key}%5C%22%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&"
         request1 = requests.post(link1, data1, headers={"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"})
@@ -50,17 +55,29 @@ def zip_file_refresher():
             data2 = f"f.req=%5B%5B%5B%22dnv2s%22%2C%22%5B%5B%5C%22{returned_id}%5C%22%5D%5D%22%2Cnull%2C%22generic%22%5D%5D%5D&"
             request2 = requests.post(link2, data2, headers={"Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"})
             response2_text = request2.text
+            print(response2_text)
 
         if response2_text.find("https://storage.googleapis.com/photos-web-downloads-anonymous/") == -1:
             raise ResponseError(f"uh google sent stuff that seemed like it was fine but they never actually sent me the link to the zip file, what?\nhere's the id i got{returned_id}\nand here's the previous response: {response2_text}")
         else:
-            with open("config.json") as the_file:
+            with open("config.json", "r") as the_file:
                 the_config = json.load(the_file)
                 ZIP_FILE_LINK = re.findall(r'https:\/\/storage\.googleapis\.com\/photos-web-downloads-anonymous\/[0-9a-zA-Z-.\/]+', response2_text)[0]
                 the_config["zip_file_link"] = ZIP_FILE_LINK
-                json.dump(the_config, the_file)
+            with open("config.json", "w") as the_file2:
+                json.dump(the_config, the_file2)
+
+        remote_images =[]
+        remote_zip = RemoteZip(ZIP_FILE_LINK)
+        raw_remote_images = remote_zip.filelist
+        for image in raw_remote_images:
+            if image.filename[-3:] in ("jpg", "png"):
+                remote_images.append(image.filename)
+        print(remote_images)
     except Exception as e:
         print(f"oh no oh dear something went wrong in the zip file refresher, here's the error: {e}")
+    finally:
+        is_zip_file_refresher_running = False
 
 
 readonly_fs = False
@@ -120,18 +137,30 @@ def get_image():
     #new_image_list = []
     #for image in image_list:
     #    new_image_list.append(f"./assets/{image}")
-    big_image_list = local_images + remote_images
-    image = random.choice(big_image_list)
-    if not readonly_fs:
-        if image[image.find("/")+1:] not in local_images:
-            remote_zip.extract(image, f"./assets/fetched")
-            os.rename(f"./assets/fetched/{image}", f"./assets/fetched/{image[image.find("/")+1:]}")
-        return send_file(f"./assets/fetched/{image[image.find("/")+1:]}", mimetype="image")
-    else:
-        image_data = remote_zip.read(image)
-        return send_file(io.BytesIO(image_data), mimetype="image")
+    try:
+        big_image_list = local_images + remote_images
+        image = random.choice(big_image_list)
+        if not readonly_fs:
+            if image[image.find("/")+1:] not in local_images:
+                try:
+                    remote_zip.extract(image, f"./assets/fetched")
+                    os.rename(f"./assets/fetched/{image}", f"./assets/fetched/{image[image.find("/")+1:]}")
+                except:
+                    image = random.choice(local_images)
+                    threading.Thread(target=zip_file_refresher).start()
+            return send_file(f"./assets/fetched/{image[image.find("/")+1:]}", mimetype="image")
+        else:
+            try:
+                image_data = remote_zip.read(image)
+                return send_file(io.BytesIO(image_data), mimetype="image")
+            except:
+                threading.Thread(target=zip_file_refresher).start()
+                return send_file(image, mimetype="image")
+    except:
+        threading.Thread(target=zip_file_refresher).start()
+        return send_file("./assets/fallback.png", "image")
 
-    return #send_file(io.BytesIO(requests.get(random.choice(local_images)).content), mimetype="image") # send_file(random.choice(new_image_list), mimetype="image")
+    #return #send_file(io.BytesIO(requests.get(random.choice(local_images)).content), mimetype="image") # send_file(random.choice(new_image_list), mimetype="image")
 
 @app.route("/")
 def index():
@@ -153,9 +182,9 @@ def do_selenium_getting(): # Might not use because vercel can't use selenium
         counter += 1
 """
 
-zip_file_refresher()
+#zip_file_refresher()
 
-#app.run()
+app.run()
 
 # regex for image links with w h at the end https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9-_]+=w[0-9]+-h[0-9]+
 # regex for all image links https:\/\/lh3\.googleusercontent\.com\/pw\/[a-zA-Z0-9-_]+
